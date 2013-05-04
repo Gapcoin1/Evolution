da515cab7cf6ae6beba934c9623c3bafb76a1829
Jonny Frey <j0nn9.fr39@gmail.com>
2013-05-04 13:45:12 +0200

    added test for onely one thread

--- test/pthread-vs-thread-clients-tsp.rb
+++ test/pthread-vs-thread-clients-tsp.rb
@@ -5,6 +5,13 @@ pt2 = 0
 tc3 = 0
 pt3 = 0
 
+_1tc = 0
+_1pt = 0
+_1tc2 = 0
+_1pt2 = 0
+_1tc3 = 0
+_1pt3 = 0
+
 if ARGV.length != 5
   puts "#{$0} <times> <num citys> <generation limit> "
   puts "<num generations> <num threads>"
@@ -46,12 +53,45 @@ ARGV[0].to_i.times do |i|
   t = Time.now
   system "./bin/tsp_pthread-O3 #{ARGV[1]} #{ARGV[2]} #{ARGV[3]} #{ARGV[4]} 0"
   pt3 += Time.now - t
+
+  t = Time.now
+  system "./bin/tsp #{ARGV[1]} #{ARGV[2]} #{ARGV[3]} 1 0"
+  _1tc += Time.now - t
+
+  t = Time.now
+  system "./bin/tsp_pthread #{ARGV[1]} #{ARGV[2]} #{ARGV[3]} 1 0"
+  _1pt += Time.now - t
+
+
+  t = Time.now
+  system "./bin/tsp-O2 #{ARGV[1]} #{ARGV[2]} #{ARGV[3]} 1 0"
+  _1tc2 += Time.now - t
+
+  t = Time.now
+  system "./bin/tsp_pthread-O2 #{ARGV[1]} #{ARGV[2]} #{ARGV[3]} 1 0"
+  _1pt2 += Time.now - t
+
+
+  t = Time.now
+  system "./bin/tsp-O3 #{ARGV[1]} #{ARGV[2]} #{ARGV[3]} 1 0"
+  _1tc3 += Time.now - t
+
+  t = Time.now
+  system "./bin/tsp_pthread-O3 #{ARGV[1]} #{ARGV[2]} #{ARGV[3]} 1 0"
+  _1pt3 += Time.now - t
 end
 
 
-puts "Thread Client O0: #{tc  /  ARGV[0].to_i}"
-puts "Thread Client O2: #{tc2 /  ARGV[0].to_i}"
-puts "Thread Client O3: #{tc3 /  ARGV[0].to_i}"
-puts "PThreads O0:      #{pt  /  ARGV[0].to_i}"
-puts "PThreads O2:      #{pt2 /  ARGV[0].to_i}"
-puts "PThreads O3:      #{pt3 /  ARGV[0].to_i}"
+puts "[#{ARGV[4]}]Thread Client O0: #{tc  /  ARGV[0].to_i}"
+puts "[#{ARGV[4]}]Thread Client O2: #{tc2 /  ARGV[0].to_i}"
+puts "[#{ARGV[4]}]Thread Client O3: #{tc3 /  ARGV[0].to_i}"
+puts "[#{ARGV[4]}]PThreads O0:      #{pt  /  ARGV[0].to_i}"
+puts "[#{ARGV[4]}]PThreads O2:      #{pt2 /  ARGV[0].to_i}"
+puts "[#{ARGV[4]}]PThreads O3:      #{pt3 /  ARGV[0].to_i}"
+
+puts "[1]Thread Client O0: #{_1tc  /  ARGV[0].to_i}"
+puts "[1]Thread Client O2: #{_1tc2 /  ARGV[0].to_i}"
+puts "[1]Thread Client O3: #{_1tc3 /  ARGV[0].to_i}"
+puts "[1]PThreads O0:      #{_1pt  /  ARGV[0].to_i}"
+puts "[1]PThreads O2:      #{_1pt2 /  ARGV[0].to_i}"
+puts "[1]PThreads O3:      #{_1pt3 /  ARGV[0].to_i}"
