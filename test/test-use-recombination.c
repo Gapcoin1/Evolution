0f99065d3d16007cf330ad25cd89b3cb431a210b
Jonny Frey <j0nn9.fr39@gmail.com>
2013-11-06 02:18:55 +0100

    updated tests

--- test/test-use-recombination.c
+++ test/test-use-recombination.c
@@ -9,13 +9,16 @@
 #define NULL 0
 #endif
 
-#include "../src/Evolution.c"
+#include "../src/evolution.h"
 
 typedef struct {
   int a, b, c, d, e, f;
 } Vektor;
 
-void *init_v() {
+void *init_v(void *opts) {
+  
+  (void) opts;
+
   Vektor *v = (Vektor *) malloc(sizeof(Vektor));
   v->a = (rand() % 100) - 50;
   v->b = (rand() % 100) - 50;
@@ -26,7 +29,10 @@ void *init_v() {
   return v;
 }
 
-void clone_v(void *dst, void *src) {
+void clone_v(void *dst, void *src, void *opts) {
+  
+  (void) opts;
+
   Vektor *vd = (Vektor *) dst;
   Vektor *vs = (Vektor *) src;
 
@@ -38,14 +44,23 @@ void clone_v(void *dst, void *src) {
   vd->f = vs->f;
 }
 
-void free_v(void *dst) {
+void free_v(void *dst, void *opts) {
+  
+  (void) opts;
+
   free((Vektor *) dst);
 }
 
-void recombinate_v(Individual *src1, Individual *src2, Individual *dst) {
-  Vektor *vs1 = (Vektor *) src1->individual;
-  Vektor *vs2 = (Vektor *) src2->individual;
-  Vektor *vd  = (Vektor *) dst->individual;
+void recombinate_v(Individual *src1, 
+                   Individual *src2, 
+                   Individual *dst, 
+                   void *opts) {
+  
+  (void) opts;
+
+  Vektor *vs1 = (Vektor *) src1->iv;
+  Vektor *vs2 = (Vektor *) src2->iv;
+  Vektor *vd  = (Vektor *) dst->iv;
 
   vd->a = vs1->a;
   vd->b = vs1->b;
@@ -55,19 +70,25 @@ void recombinate_v(Individual *src1, Individual *src2, Individual *dst) {
   vd->f = vs2->f;
 }
 
-void mutate_v(Individual *src) {
-  Vektor *v = (Vektor *) src->individual;
+void mutate_v(Individual *src, void *opts) {
+  
+  (void) opts;
 
-    v->a += (rand() % 3) - 1;
-    v->b += (rand() % 3) - 1;
-    v->c += (rand() % 3) - 1;
-    v->d += (rand() % 3) - 1;
-    v->e += (rand() % 3) - 1;
-    v->f += (rand() % 3) - 1;
+  Vektor *v = (Vektor *) src->iv;
+
+  v->a += (rand() % 3) - 1;
+  v->b += (rand() % 3) - 1;
+  v->c += (rand() % 3) - 1;
+  v->d += (rand() % 3) - 1;
+  v->e += (rand() % 3) - 1;
+  v->f += (rand() % 3) - 1;
 }
 
-int fittnes_v(Individual *src) {
-  Vektor *v = (Vektor *) src->individual;
+int64_t fittnes_v(Individual *src, void *opts) {
+  
+  (void) opts;
+
+  Vektor *v = (Vektor *) src->iv;
 
   return abs(v->a - v->b) + abs(v->b - v->c)
             + abs(v->c - v->d) + abs(v->d - v->e)
@@ -82,12 +103,27 @@ int main(int argc, char *argv[]) {
 
   Individual *best;
 
-  Evolution *ev = new_evolution(init_v, clone_v, free_v, mutate_v, fittnes_v,
-                          recombinate_v, NULL, TEST_NUM_IVS, atoi(argv[1]), 1.0, 0.5,
-                             EV_UREC|EV_UMUT|EV_AMUT|EV_KEEP);
+  EvInitArgs args;
+
+  args.init_iv              = init_v;
+  args.clone_iv             = clone_v;
+  args.free_iv              = free_v;
+  args.mutate               = mutate_v;
+  args.fitness              = fittnes_v;
+  args.recombinate          = recombinate_v;
+  args.clone_iv             = NULL;
+  args.population_size      = TEST_NUM_IVS;
+  args.generation_limit     = atoi(argv[1]);
+  args.mutation_propability = 1.0;
+  args.death_percentage     = 0.5;
+  args.opts                 = NULL;
+  args.num_threads          = 1;
+  args.flags                = EV_UREC|EV_UMUT|EV_AMUT|EV_KEEP;
+
+  Evolution *ev = new_evolution(&args);
   best = evolute(ev);
 
-  Vektor *v = (Vektor *) best->individual;
+  Vektor *v = (Vektor *) best->iv;
 
   printf("Best Vektor: (%3d, %3d, %3d, %3d, %3d, %3d) %10li\n\n", v->a, v->b, v->c, v->d, v->e, v->f, 
             best->fitness);
@@ -95,7 +131,7 @@ int main(int argc, char *argv[]) {
   int i;
   for (i = 0; i < TEST_NUM_IVS; i++) {
     best = ev->population[i];
-    v = (Vektor *) best->individual;
+    v = (Vektor *) best->iv;
 
     printf("(%3d, %3d, %3d, %3d, %3d, %3d) %10li\n", v->a, v->b, v->c, v->d, v->e, v->f, 
               best->fitness);
@@ -103,4 +139,3 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
-
