a9254c6dfeb5765290eeaca53e035689b8006766
Jonny Frey <j0nn9.fr39@gmail.com>
2013-11-06 16:11:04 +0100

    improved test, now threadable rand is used

--- test/last_test.c
+++ test/last_test.c
@@ -3,26 +3,32 @@
  * (every function runns in O(n))
  */
 #include "../src/evolution.h"
+#include "../src/C-Utils/Rand/src/rand.h"
+
+typedef struct {
+  int length;
+  int i;
+} ThreadArgs;
 
 void *init_v(void *opts) {
 
-  int *length = opts;
+  ThreadArgs *args = opts;
   int i;
 
-  int *ary = malloc(sizeof(int) * *length);
+  int *ary = malloc(sizeof(int) * args->length);
 
-  for (i = 0; i < *length; i++)
-    ary[i] = rand() - (RAND_MAX / 2);
+  for (i = 0; i < args->length; i++)
+    ary[i] = rand32(args->i) - (RAND_MAX / 2);
 
   return ary;
 }
 
 void clone_v(void *dst, void *src, void *opts) {
 
-  int *length = opts;
+  ThreadArgs *args = opts;
   int i;
 
-  for (i = 0; i < *length; i++)
+  for (i = 0; i < args->length; i++)
     ((int *) dst)[i] = ((int *) src)[i];
 }
 
@@ -37,36 +43,36 @@ void recombinate_v(Individual *src1,
                    void *opts) {
 
   int i;
-  int *length = opts;
+  ThreadArgs *args = opts;
   int *vs1 = (int *) src1->iv;
   int *vs2 = (int *) src2->iv;
   int *vd  = (int *) dst->iv;
 
-  for (i = 0; i < *length; i++)
-    vd[i] = (rand() % 2) ? vs1[i] : vs2[i];
+  for (i = 0; i < args->length; i++)
+    vd[i] = (rand32(args->i) % 2) ? vs1[i] : vs2[i];
   
 }
 
 void mutate_v(Individual *src, void *opts) {
 
-  int *length = opts;
+  ThreadArgs *args = opts;
   int i, r, *ary = src->iv;
 
-  for (i = 0; i < *length; i++) {
-    r = (rand() % 3) - 1;
+  for (i = 0; i < args->length; i++) {
+    r = (rand32(args->i) % 3) - 1;
 
-    ary[i] = (rand() % 100) ? ary[i] : ary[i] + r;
+    ary[i] = (rand32(args->i) % 100) ? ary[i] : ary[i] + r;
   }
 
 }
 
 int64_t fittnes_v(Individual *src, void *opts) {
 
-  int *length = opts;
+  ThreadArgs *args = opts;
   int i, *ary = src->iv;
   int64_t max = 0;
 
-  for (i = 0; i < *length; i++)
+  for (i = 0; i < args->length; i++)
     max += (ary[i] < 0) ? ary[i] * -1 : ary[i];
 
   return max;
@@ -85,6 +91,8 @@ int main(int argc, char *argv[]) {
   int verbose = EV_VEB0;
   int  n_threads = atoi(argv[2]);
 
+  init_rand32(n_threads);
+
   switch (atoi(argv[3])) {
     case 1:   verbose = EV_VEB1; break;
     case 2:   verbose = EV_VEB2; break;
@@ -93,11 +101,12 @@ int main(int argc, char *argv[]) {
   }
 
   Individual *best;
-  int **opts = malloc(sizeof(int *) * n_threads);
+  ThreadArgs **opts = malloc(sizeof(ThreadArgs *) * n_threads);
   int i;
   for (i = 0; i < n_threads; i++) {
-    opts[i] = malloc(sizeof(int));
-    *opts[i] = length;
+    opts[i] = malloc(sizeof(ThreadArgs));
+    opts[i]->length = length;
+    opts[i]->i      = i;
   }
 
   EvInitArgs args;
